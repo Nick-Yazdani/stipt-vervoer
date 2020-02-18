08b8e020875d93ae7cff5c42ef6cd3afb00db850
Nick Yazdani <nnyazdani92@gmail.com>
2020-02-19 03:47:36 +1100

    form rework

--- src/components/contact-form-component/contact-form.component.jsx
+++ src/components/contact-form-component/contact-form.component.jsx
@@ -5,23 +5,33 @@ import './contact-form.component.styles.css';
 
 class ContactForm extends Component {
     state = {
+        bedrijfsnaam: '',
         naam: '',
-        bedrijf: '',
+        adres: '',
+        postcode: '',
+        plaats: '',
+        telefoon: '',
+        fax: '',
         email: '',
-        vervoer: '',
-        wensen: '',
+        bericht: '',
         errorStyle: { borderBottom: '1px solid red' },
+        textareaErrorStyle: {border: '1px solid red'},
         nameError: false,
-        emailError: false
+        emailError: false,
+        berichtError: false
     }
 
 
     postData = async () => {
         const naam = this.state.naam;
         const email = this.state.email;
-        const bedrijf = this.state.bedrijf;
-        const vervoer = this.state.vervoer;
-        const wensen = this.state.wensen;
+        const bedrijfsnaam = this.state.bedrijfsnaam;
+        const adres = this.state.adres;
+        const postcode = this.state.postcode;
+        const plaats = this.state.plaats;
+        const telefoon = this.state.telefoon;
+        const fax = this.state.fax;
+        const bericht = this.state.bericht;
         // Default options are marked with *
         const response = await fetch('https://stiptvervoer.com/contact', {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
@@ -34,7 +44,7 @@ class ContactForm extends Component {
             },
             redirect: 'follow', // manual, *follow, error
             referrerPolicy: 'no-referrer', // no-referrer, *client
-            body: JSON.stringify({ naam, email, bedrijf, vervoer, wensen }) // body data type must match "Content-Type" header
+            body: JSON.stringify({ naam, email, bedrijfsnaam, adres, postcode, plaats, telefoon, fax, bericht }) // body data type must match "Content-Type" header
         });
         console.log(response);
         return await response.json();
@@ -58,10 +68,16 @@ class ContactForm extends Component {
         else {
             this.setState({ emailError: false })
         }
+        if (this.state.bericht === '') {
+            this.setState({ berichtError: true });
+        }
+        else {
+            this.setState({ berichtError: false })
+        }
 
-        if (this.state.name !== '' && this.state.email !== '') {
+        if (this.state.name !== '' && this.state.email !== '' && this.state.bericht !== '') {
             this.postData().then(() => {
-                this.setState({ naam: '', bedrijf: '', email: '', vervoer: '', wensen: '' })
+                this.setState({ naam: '', bedrijfsnaam: '', email: '', adres: '', postcode: '', plaats: '', telefoon: '', fax: '', bericht: ''  })
             })
         }
     }
@@ -71,12 +87,17 @@ class ContactForm extends Component {
             <div className="form-container">
                 <div className="overlay" style={{ height: "120vh" }}>
                     <form onSubmit={this.handleSubmit}>
-                        <FormInput name="naam" type="text" text="uw naam (verplicht)" handleChange={this.handleChange} value={this.state.naam} style={this.state.nameError ? this.state.errorStyle : null} />
-                        <FormInput name="bedrijf" type="text" text="uw bedrijf (optioneel)" handleChange={this.handleChange} value={this.state.bedrijf} />
-                        <FormInput name="email" type="email" text="uw email (verplicht)" handleChange={this.handleChange} value={this.state.email} style={this.state.emailError ? this.state.errorStyle : null} />
+                        <FormInput name="bedrijfsnaam" type="text" text="uw bedrijf (optioneel)" handleChange={this.handleChange} value={this.state.bedrijfsnaam} />
+                        <FormInput name="naam" type="text" text="naam*" handleChange={this.handleChange} value={this.state.naam} style={this.state.nameError ? this.state.errorStyle : null} />
+                        <FormInput name="adres" type="text" text="adres" handleChange={this.handleChange} value={this.state.adres} />
+                        <FormInput name="postcode" type="text" text="postcode" handleChange={this.handleChange} value={this.state.postcode} />
+                        <FormInput name="plaats" type="text" text="plaats" handleChange={this.handleChange} value={this.state.plaats} />
+                        <FormInput name="telefoon" type="text" text="telefoon" handleChange={this.handleChange} value={this.state.telefoon} />
+                        <FormInput name="fax" type="text" text="fax" handleChange={this.handleChange} value={this.state.fax} />
+                        <FormInput name="email" type="email" text="email*" handleChange={this.handleChange} value={this.state.email} style={this.state.emailError ? this.state.errorStyle : null} />
                         <div className="text-control">
-                            <label htmlFor="wensen">OMSCHRIJF UW WENSEN EN IDEEËN (B.V. CATERING AAN BOORD)</label>
-                            <textarea name="wensen" type="text" onChange={this.handleChange} value={this.state.wensen}></textarea>
+                            <label htmlFor="bericht">BERICHT*</label>
+                            <textarea name="bericht" type="text" onChange={this.handleChange} value={this.state.bericht} style={this.state.berichtError ? this.state.textareaErrorStyle : null}></textarea>
                         </div>
                         <div className="button-control">
                             <button>verzenden</button>
