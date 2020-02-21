7f727e48836bb1fd7067650927c9cd18dbbb0245
Nick Yazdani <nnyazdani92@gmail.com>
2020-02-21 19:07:45 +1100

    added default image

--- src/components/busreizen-component/card-component/card.component.jsx
+++ src/components/busreizen-component/card-component/card.component.jsx
@@ -7,7 +7,8 @@ class Card extends React.Component {
     render() {
         return (
             <div className="card-parent">
-                <img className="img" src={this.props.image} alt={this.props.alt} />
+                {/* <img className="img" src={this.props.image} alt={this.props.alt} /> */}
+                <img vlsddName="img" src="https://i.ibb.co/9pGLBjQ/lukas-souza-b-AFWnh-GAvxk-unsplash-min.jpg" alt="lukas-souza-b-AFWnh-GAvxk-unsplash-min" border="0" />
                 <div className="span-div"><span className="span-caption">{this.props.caption}</span></div>
                 <Link to={"/busreizen/" + this.props.param}>MEER INFORMATIE</Link>
             </div>
